b4aa13a3d0306caa3f8174148cebfbab9a0a5139
minako-aino <79149650+minako-aino@users.noreply.github.com>
2021-05-11 23:27:26 +0300

    Update aircraft.cpp

--- aircraft.cpp
+++ aircraft.cpp
@@ -11,6 +11,7 @@ class AircraftCarrier;
 class NavigationSystem;
 class Engine;
 class Airplane;
+class Captain;
 
 class Tank {
     int level;
@@ -42,20 +43,23 @@ class Sailor {
     string name;
     public:
         Sailor(string data) { name = data; }
-        float check(FuelSupplySystem& fss);
-        void refuel(FuelSupplySystem& fss);
-        void startengine(AircraftCarrier& aircraft);
-        void stopengine(AircraftCarrier& aircraft);
-        void action(AircraftCarrier& aircraft);
+        float check(FuelSupplySystem& fss);             // перевірка к-сті палива
+        void refuel(FuelSupplySystem& fss);             // заправити 
+        void startengine(AircraftCarrier& aircraft);    // запуск двигунів
+        void stopengine(AircraftCarrier& aircraft);     // вимкнення двигунів вручну
+        void action(AircraftCarrier& aircraft);         // виконання додатквих дій
+        // змінити швидкість подачі палива
+        void setfuelspeed(Captain& captain, FuelSupplySystem& fss);
 };
 
 class Captain {
     string name ;
     public:
         Captain(string data) { name = data; }
-        void setcourse(NavigationSystem& ns);
-        void checkcourse(NavigationSystem& ns);
-        void setspeed(Engine& engine);
+        void setcourse(NavigationSystem& ns);           // встановлення курсу корабля
+        void checkcourse(NavigationSystem& ns);         // перевірка курсу
+        void setspeed(Engine& engine);                  // встановлення швидкості корабля
+        int setfuelspeed();                             // встановлення швидкості подачі палива
 };
 
 class NavigationSystem {
@@ -71,9 +75,11 @@ class Engine {
     public:
         Engine() { power = 7000;
                    speed = 0;
-                   state = false; }
+                   state = false;
+                   fuelspeed = 0; }
         int power;
         int speed;
+        int fuelspeed;
         
         bool checkstate() { return state; };
         bool rotate();
@@ -81,17 +87,17 @@ class Engine {
 
 class FuelSupplySystem {
     public:
-        bool givingstate;
-        Tank tank;
+        bool givingstate;               // стан подачі палива
+        Tank tank;           
         Engine engine;
-        bool givefuel();
+        bool givefuel();                // функція подачі палива
 };
 
 class Generator {
     public:
         Generator() { state = false; }
-        bool state;
-        int makeenergy();
+        bool state;                     // стан генератора
+        bool makeenergy();
 };
 
 class ElectroSystem {
@@ -106,34 +112,43 @@ class Pilot {
     string name;
     public:
         Pilot(string data) { name = data; }
+        // керування польоту
         void fly(Airplane& plane, RunwaySystem& rs, int i);
+        // приземлення
         void land(Airplane& plane, RunwaySystem& rs, int i);
+        // регестрація нового літака
         int regist(AircraftCarrier& aircraft, int i);
+        // перевірка наявності ID у літака
         int checkID(AircraftCarrier& aircraft, int i);
+        // резервування смуги
         int reserve(AircraftCarrier& aircraft, int plane, int i);
-        int checknumber(AircraftCarrier& aircraft, int plane, int i);
+        // перевірка наявності резервації смуги
+        int checknumber(AircraftCarrier& aircraft, int plane, int i, int m);
 };
 
 class Airplane {
     public:
+        // за замовчуванням - очікування
         Airplane() { state = "waiting";}
         int ID;
-        string name;
-        string state;
-        void fly();
+        string name;                // назва літака
+        string state;               // стан
+        void fly();                 // політ
 };
 
 class AccountingSystem {
     public:
-        map <int, int> RegisterBook;
-        int setID(Airplane& airplane);
-        void reverserunway();
+        map <int, int> RegisterBook;            // книга регестрації
+        int setID(Airplane& airplane);          // видання ID
         bool getrunaway(int plane);
 };
 
 class AircraftCarrier {
     public:
+        // літаки, що підпорядковані авіаносцю
         Airplane planes[20];
+
+        // екземпляри кожної із підсистем
         AccountingSystem accountingsystem;
         FuelSupplySystem fuelsupplysystem;
         RunwaySystem runwaysystem;
@@ -163,10 +178,10 @@ void RunwaySystem::setrunwaystate(int index, int state) {
 }
 
 float Sailor::check(FuelSupplySystem& fss) {
-	int volume = fss.tank.getvolume()/100;
-	int level = fss.tank.getlevel()/100;
-    float perc = (100/volume)*level;
-    if (perc < 50) refuel(fss);
+	int volume = fss.tank.getvolume()/100;  // для більш чітких розрахунків
+	int level = fss.tank.getlevel()/100;      
+    float perc = (100/volume)*level;        // розрахування відсотків
+    if (perc < 50) refuel(fss);             // заправити, якщо залишилось менше половини
     return perc;
 }
 
@@ -176,20 +191,37 @@ void Sailor::refuel(FuelSupplySystem& fss) {
     cout << " -> Current state: " << check(fss) << endl;
 }
 
+void Sailor::setfuelspeed (Captain& captain, FuelSupplySystem& fss) {
+    fss.engine.fuelspeed = captain.setfuelspeed();
+    cout << " -> Current fuel speed: " << fss.engine.fuelspeed << endl;
+    if (fss.engine.fuelspeed == 0) {
+        cout << "Engines were stopped\n";
+    }
+}
+
+int Captain::setfuelspeed() {
+    int speed;
+    cout << "Please set fuel speed\n";
+    cin >> speed;
+    return speed;
+}
+
 void Captain::setcourse(NavigationSystem& ns) {
-    ns.setcourse();
+    ns.setcourse();         // взаємодія із навігаційною системою
 }
 
 void Captain::checkcourse(NavigationSystem& ns) {
+    // взаємодія із навігаційною системою
     cout << " -> Current destination: " << ns.getcourse() << endl;
 }
 
 void Captain::setspeed(Engine& engine) {
-    if (engine.checkstate()) {
+    if (engine.checkstate()) {      // встановити нову швидкість можна лише якщо двигуни працюють
         cout << "Enter the speed: ";
         cin >> engine.speed;
     }
     cout << " -> Engine speed: " << engine.speed << endl;
+    if (engine.speed == 0 && engine.checkstate()) engine.rotate(); // вимкнення двигунів, якщо була встановлена швидкість 0
 }
 
 bool Engine::rotate() {
@@ -237,7 +269,7 @@ void Sailor::action(AircraftCarrier& aircraft) {
     int answ;
 
     do{
-        cout << "What do you want to do?\n1. Check and fix fuel supply system\n2. Stop aircraft\n";
+        cout << "What do you want to do?\n1. Check and fix fuel supply system\n2. Stop aircraft\n3. Exit\n";
         cin >> answ;
         switch(answ) {
             case 1: {
@@ -247,14 +279,22 @@ void Sailor::action(AircraftCarrier& aircraft) {
                 if (aircraft.fuelsupplysystem.engine.checkstate()) stopengine(aircraft);
                 else cout << "It is already stopped\n";
                 break; }
+            case 3: {
+				if (aircraft.fuelsupplysystem.engine.checkstate()) stopengine(aircraft); 
+				cout << "It`s all for today\n";
+                break; }
             default: cout << "Wrong symbol\n";
         }
     } while (answ == 1 || answ == 2);
 }
 
+bool Generator::makeenergy(){
+    state = true;
+    return state;
+}
+
 void ElectroSystem::giveenergy() {
-    generator.state = true;
-    cout << "Generator is working\n";
+    if (generator.makeenergy()) cout << "Generator is working\n";
 }
 
 int Pilot::checkID(AircraftCarrier& aircraft, int i) {
@@ -263,7 +303,7 @@ int Pilot::checkID(AircraftCarrier& aircraft, int i) {
 
     cout << "Which plane are you looking for?\nPlease enter the ID\n";
     cin >> answ;
-
+    // регестація розпочинається у випадку, коли не було знайдено потрібного ID у обліковій книзі
     if ( aircraft.accountingsystem.RegisterBook.find(answ) == aircraft.accountingsystem.RegisterBook.end()) {
         cout << "There are no plane with this ID\n";
         return regist(aircraft, i);
@@ -280,24 +320,34 @@ int Pilot::regist(AircraftCarrier& aircraft, int i) {
     }
 }
 
-int Pilot::checknumber(AircraftCarrier& aircraft, int plane, int i) {
+int Pilot::checknumber(AircraftCarrier& aircraft, int plane, int i, int m) {
     cout << "Searching for the reserved runway...\n";
+    // резервація розпочинається у випадку, якщо не було знайдено відповідного запису в обліковій книзі
     if (aircraft.accountingsystem.getrunaway(plane)) {
         cout << "This plane doesn`t have a reserved runway\n";
         cout << "Starting the registration\n";
-        cout << " -> Reserved runway: " << reserve(aircraft, plane, i) << endl;
+        cout << " -> Reserved runway: " << reserve(aircraft, plane, m) << endl;
+    } else {
+        // якщо ж знайдено - продовжуємо роботу із даними
+    	aircraft.runwaysystem.setrunwaystate(i, 1);
+        cout << " -> Reserved runway: " << aircraft.accountingsystem.RegisterBook[plane] << endl;
     }
+    aircraft.runwaysystem.setrunwaystate(i, 1);
+    return aircraft.accountingsystem.RegisterBook[plane];
 }
 
 int Pilot::reserve(AircraftCarrier& aircraft, int plane, int i) {
+    // обмеження у 20 смуг
     if (i < 20) {
         aircraft.accountingsystem.RegisterBook[plane] = i;
         aircraft.runwaysystem.setrunwaystate(i, 1);
         return i;
-    } else cout << "There are no available parking places\n";    
+    } else cout << "There are no available parking places\n";
 }
 
 void Pilot::land(Airplane& plane, RunwaySystem& rs, int i) {
+	rs.setrunwaystate(i, 1);
+    // початок лише у випадку якщо смуга має стан "відкрито"
     if (rs.runways[i].state == rs.runways[i].states_list[1]) {
         rs.setrunwaystate(i, 3);
         cout << " -> Current runway state: " << rs.runways[i].state << endl;
@@ -310,10 +360,12 @@ void Pilot::land(Airplane& plane, RunwaySystem& rs, int i) {
 }
 
 void Pilot::fly(Airplane& plane, RunwaySystem& rs, int i) {
+    // після приземлення стан смуги був змінений на "закрито", тому для взлету необхідно повернутися до стану "відкрито"
     if (rs.runways[i].state == rs.runways[i].states_list[0]) {
         rs.setrunwaystate(i, 1);
         cout << " -> Current runway state: " << rs.runways[i].state << endl; 
     } else cout << "Runway is not ready\n";
+    // політ ише у випадку якщо смуга відкрита
     if (rs.runways[i].state == rs.runways[i].states_list[1]) {
         plane.fly();
         rs.setrunwaystate(i, 0);
@@ -333,10 +385,12 @@ int AccountingSystem::setID(Airplane& airplane){
 
     cout << "Starting the registration\n";
     do {
+        // генерація 6-значного числа згідно вимог до ID 
         ID = random(100000,999999);
-      	
+      	// якщо у книзі не знайдено потрібних данних, додаємо нові
         if (RegisterBook.find(ID) == RegisterBook.end()) {
             airplane.ID = ID;
+            // додання у книгу
             RegisterBook.insert(pair<int, int>(ID, -1));
         }
     } while (RegisterBook.find(ID) == RegisterBook.end());
@@ -360,6 +414,7 @@ int main(){
     Pilot pilot01("Ren Tao");
     char ans;
     int planeID;
+    int runwayID;
     
     // aircraft
     // sailor works
@@ -371,14 +426,16 @@ int main(){
     captain.checkcourse(aircraft0.navigationsystem);
     captain.setcourse(aircraft0.navigationsystem);
     captain.setspeed(aircraft0.fuelsupplysystem.engine);
-
-    do {
-        int i = 0;
+    sailor.setfuelspeed(captain, aircraft0.fuelsupplysystem);
+	
+	int i = 0;
+	int m = 0;
+    do {    
         planeID = pilot01.checkID(aircraft0, i);
-        pilot01.checknumber(aircraft0, planeID, i);
-        pilot01.land(aircraft0.planes[i], aircraft0.runwaysystem, i);
+        runwayID = pilot01.checknumber(aircraft0, planeID, i, m++);
+        pilot01.land(aircraft0.planes[i], aircraft0.runwaysystem, aircraft0.accountingsystem.RegisterBook[runwayID]);
         pilot01.fly(aircraft0.planes[i], aircraft0.runwaysystem, i);
-        i++;
+        //i++;
         cout << "Are you interested in other planes?\n";
         cin >> ans;
     } while (ans == 'y' || ans == 'Y');
